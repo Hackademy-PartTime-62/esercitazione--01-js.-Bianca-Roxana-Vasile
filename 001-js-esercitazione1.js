60d2a1b910c68bdc6587f85edfc9091b40e09ab5
Bianca Vasile <bubu87.bvv@gmail.com>
2025-06-14 21:12:50 +0200

    esercitazione-001

---
+++ 001-js-esercitazione1.js
@@ -0,0 +1,37 @@
+// alert("ciao a tutti")
+
+let numero1;
+numero1 = 10;
+console.log(numero1 );
+
+let numero2;
+numero2 = 15;
+console.log(numero2 );
+
+let la_stringa;
+la_stringa =("ciao a tutti");
+console.log(la_stringa);
+
+la_stringa ="20";
+console.log(la_stringa);
+
+const PI_greco=3.14159265359;
+console.log(PI_greco);
+PI_greco=42;
+console.log(PI_greco);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  non possiamo modificare il valore di una costante, per il fatto che ci  viene generato un errore bloccante
